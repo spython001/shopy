532d562b30959e95b402f3976f8fac9179d999a6
spython <emmanuelegbo01@gmail.com>
2023-12-14 14:15:48 +0100

    added packages

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='app'>
+      app
+    </div>
+  )
+}
+
+export default App
---
+++ vite.config.js
@@ -0,0 +1,34 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+  },
+  host: true,
+  },
+  optimizeDeps: {
+    include: ['*.js'], // Include all .js files in your project
+  },
+  esbuild: {
+    loader: "jsx",
+    include: [
+      // Business as usual for .jsx and .tsx files
+      "src/**/*.jsx",
+      "node_modules/**/*.jsx",
+      // --- OR ---
+
+      // Add these lines to allow all .js files to contain JSX
+      "src/**/*.js",
+      "node_modules/**/*.js",
+    ],
+    exclude: [],
+  }
+})
