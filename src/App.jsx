65488fa4addcaabee2124ca03ff2f7ebc9b6a907
spython <emmanuelegbo01@gmail.com>
2023-12-16 16:32:23 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Topbar from './components/topbar/Topbar'
+import Product from './pages/product/Product'
 
 function App() {
 
   return (
     <div className='app'>
       <Topbar />
+      <Product />
     </div>
   )
 }
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -2,6 +2,16 @@ import './topbar.scss'
 
 export default function Topbar() {
   return (
-    <div className='top'>Topbar</div>
+    <nav className='top'>
+        <div className="wrapper">
+            <input type="text" placeholder='Enter your search ...' className='searchInput'/>
+        </div>
+
+        <div className="profileContainer">
+            <a href=""><i className="navIcon fa-regular fa-heart"></i></a>
+            <a href=""><i className="navIocn fa-solid fa-cart-plus"></i></a>
+            <a href=""><i className="navIcon fa-regular fa-user"></i></a>
+        </div>
+    </nav>
   )
 }
