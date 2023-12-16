df4bdf4143ddc024ee9cbf550170cb72613b6108
spython <emmanuelegbo01@gmail.com>
2023-12-16 16:33:40 +0100

    modified topbar folder

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -2,14 +2,14 @@ import './topbar.scss'
 
 export default function Topbar() {
   return (
-    <nav className='top'>
-        <div className="wrapper">
+    <nav className='topbar'>
+        <div className="inputContainer">
             <input type="text" placeholder='Enter your search ...' className='searchInput'/>
         </div>
 
         <div className="profileContainer">
             <a href=""><i className="navIcon fa-regular fa-heart"></i></a>
-            <a href=""><i className="navIocn fa-solid fa-cart-plus"></i></a>
+            <a href=""><i className="navIcon fa-solid fa-cart-plus"></i></a>
             <a href=""><i className="navIcon fa-regular fa-user"></i></a>
         </div>
     </nav>
