35725d6040294da3a43af07a20ae10d9fb658be4
spython <emmanuelegbo01@gmail.com>
2023-12-16 16:48:20 +0100

    created sidebar folder

--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -2,6 +2,8 @@ import './product.scss'
 
 export default function Product() {
   return (
-    <div>Product</div>
+    <div className='cardContainer'>
+      <img src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg" alt="" />
+    </div>
   )
 }
