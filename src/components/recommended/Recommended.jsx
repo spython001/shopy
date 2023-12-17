16ed3f59d8b3918a20c13dbe4c68dd931aed0ff4
spython <emmanuelegbo01@gmail.com>
2023-12-17 09:34:55 +0100

    commit

--- src/components/recommended/Recommended.jsx
+++ src/components/recommended/Recommended.jsx
@@ -3,11 +3,14 @@ import './recommended.scss'
 export default function Recommended() {
   return (
     <div className='recommended'>
-        <h2>Recommended</h2>
+        <h2 className='recommendedTitle'>Recommended</h2>
 
         <div className="recommendedBtns">
             <button className='btns'>All Products</button>
             <button className="btns">Nike</button>
+            <button className="btns">Adidas</button>
+            <button className="btns">Puma</button>
+            <button className="btns">Vans</button>
         </div>
     </div>
   )
