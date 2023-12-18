6c703b12b23cf45c47924d7412a680d0f9d84f4a
spython <emmanuelegbo01@gmail.com>
2023-12-18 17:03:34 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
+/* eslint-disable react/prop-types */
+import { useEffect, useState } from 'react'
 import './App.css'
 import Recommended from './components/recommended/Recommended'
 import Sidebar from './components/sidebar/Sidebar'
 import Topbar from './components/topbar/Topbar'
+import data from './data'
 import Product from './pages/product/Product'
 
 function App() {
+  const [filtered, setFiltered] = useState([])
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const products = await data;
+      setFiltered(data);
+      console.log(products)
+    }
+    fetchProducts();
+  }, [])
+  
+
 
   return (
     <div className='app'>
